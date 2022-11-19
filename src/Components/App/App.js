e450285119e95171d932059a75438e1cfbf4d20e
Victor Romero <42779507+vsrromero@users.noreply.github.com>
2022-11-19 09:43:34 +0000

    fix spotify module, should be an object and not a function, add the search method where the search term is passed to app component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
+import Spotify from '../../util/Spotify';
 
 class App extends React.Component {
 
@@ -70,7 +71,9 @@ class App extends React.Component {
   }
 
   search(term) {
-    console.log(term);
+    Spotify.search(term).then(apiSearchResults => {
+      this.setState({searchResults: apiSearchResults})
+    })
   }
 
   render(){
--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,37 +1,68 @@
-let accessToken;
 const clientId = '18523cfcd8f648a6a94af1b19bdd5182';
 const redirectUri = 'http://localhost:3000/';
 const clientSecret = '57dfce01b5be487793d7ae3b6858175e';
+let accessToken;
 
-function getAccessToken(){
+const Spotify = {
 
-    if(accessToken){
-        return accessToken;
-    } else {
-        console.log("User access token is not set");
-    }
+    getAccessToken() {
+
+        if(accessToken){
+            return accessToken;
+        } else {
+            console.log("User access token is not set");
+        }
 
     //check for access token matches
 
-    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
-    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
 
-    if(accessTokenMatch && expiresInMatch){
+        if(accessTokenMatch && expiresInMatch){
+            
+            accessToken = accessTokenMatch[1];
+            const expiresIn = Number(expiresInMatch[1]);
+            
+            // Clear parameters and allow to grab a new access token when it expires.
+
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
+            window.history.pushState('Acess Token', null, '/');
+            return accessToken;
+
+        } else {
+            const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+            window.location = accessUrl;
+        }
         
-        accessToken = accessTokenMatch[1];
-        const expiresIn = Number(expiresInMatch[1]);
-        
-        // Clear parameters and allow to grab a new access token when it expires.
+    },
 
-        window.setTimeout(() => accessToken = '', expiresIn * 1000);
-        window.history.pushState('Acess Token', null, '/');
-        return accessToken;
+    search(term){
+        const accessToken = Spotify.getAccessToken();
+        const endpoint = 'https://api.spotify.com/v1/search?type=track&q=';
+        return fetch(endpoint+term, 
+                    { headers: { 
+                        Authorization: `Bearer ${accessToken}`
+                    } 
+                }).then(response => {
+                    return response.json(); //converts response to jason
+                }).then(jsonResponse => { //checks if response is empty, if yes, returns an empty array, if not returns the required object.
+                    if(!jsonResponse.traks) {
+                        return [];
+                    } else {
 
-    } else {
-        const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
-        window.location = accessUrl;
+                        return jsonResponse.traks.items.map(track => ({
+                            id: track.id,
+                            name: track.name,
+                            artist: track.artist[0].name,
+                            album: track.album.name,
+                            uri: track.uri
+                        }));
+                        
+                    }
+                })
+
     }
-    
+
 }
 
-//export default Spotify;
+export default Spotify;
